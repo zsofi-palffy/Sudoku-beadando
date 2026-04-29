760654c5d0c0a7fcac855bf29229840ff6a2efa3
zsofi-palffy <zso.palffy@gmail.com>
2026-04-29 22:59:06 +0200

    Added functions register_widgets and event_loop to Os_alkalmazas

--- CLionGraphicsKit/Os_alkalmazas.cpp
+++ CLionGraphicsKit/Os_alkalmazas.cpp
@@ -1,7 +1,37 @@
 #include "Os_alkalmazas.hpp"
 #include "graphics.hpp"
+#include "Widget.hpp"
+
+using namespace std;
+using namespace genv;
 
 Os_alkalmazas::Os_alkalmazas(int szelesseg, int magassag):
-                             szelesseg(szelesseg), magassag(magassag){
+                             szelesseg(szelesseg), magassag(magassag)
+{
+}
 
+void Os_alkalmazas::register_widgets(Widget* w) {
+    widgets_.push_back(w);
+}
+
+void Os_alkalmazas::event_loop() {
+    event ev;
+    int fokusz = -1;
+    while (gin >> ev) {
+        if (ev.type == ev_mouse && ev.button == btn_left) {
+            for (int i = 0; i < widgets_.size(); i++) {
+                if (widgets_[i]->jelenlegi(ev.pos_x, ev.pos_y)) {
+                    fokusz = i;
+                }
+            }
+        }
+
+        if (fokusz != -1) {
+            widgets_[fokusz]->handle(ev);
+        }
+
+        for (Widget *w: widgets_) {
+            w->rajzol();
+        }
+    }
 }
--- CLionGraphicsKit/Os_alkalmazas.hpp
+++ CLionGraphicsKit/Os_alkalmazas.hpp
@@ -1,16 +1,18 @@
 #ifndef CLIONGRAPHICSKIT_OS_ALKALMAZAS_H
 #define CLIONGRAPHICSKIT_OS_ALKALMAZAS_H
 #include "graphics.hpp"
+#include "Widget.hpp"
 
 class Os_alkalmazas {
 public:
     Os_alkalmazas(int, int);
 
-    virtual void draw();
-    virtual void handle(genv::event);
+    virtual void register_widgets(Widget* w);
+    virtual void event_loop();
 
 protected:
     int szelesseg, magassag;
+    std::vector<Widget*> widgets_;
 };
 
 
